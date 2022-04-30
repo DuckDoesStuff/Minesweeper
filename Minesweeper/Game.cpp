167f2e3235271dcaab96b75dadc8cf3b3a090b3e
DuckDoesStuff <mailtienduc@gmail.com>
2022-04-30 20:19:50 +0700

    finished randomizing mines

--- Minesweeper/Game.cpp
+++ Minesweeper/Game.cpp
@@ -10,7 +10,58 @@ Game::~Game() {
 
 void Game::setUpGame(int size) {
 	Common::clearConsole();
-	drawGame(size);
+	//drawGame(size);
+	generateGameData(size);
+	renderGameData(size);
+}
+
+void Game::generateGameData(int size)
+{
+	switch (size)
+	{
+	case 10:
+		_numOfMines = 20;
+		break;
+	case 15:
+		_numOfMines = 40;
+		break;
+	case 25:
+		_numOfMines = 100;
+	default:
+		break;
+	}
+
+	//Initializing _minesMap
+	_minesMap = new bool*[size];
+	for (int i = 0; i < size; i++) _minesMap[i] = new bool[size];
+	for (int i = 0; i < size; i++)
+		for (int j = 0; j < size; j++)
+			_minesMap[i][j] = 0;
+
+	bool* checkDuplicate = new bool[size * size];
+	for (int i = 0; i < size * size; i++) checkDuplicate[i] = 0;
+
+	srand(time(0));
+	int n = _numOfMines;
+	while (n) {
+		int i = rand() % (size * size);
+		if (!checkDuplicate[i]) {
+			checkDuplicate[i] = 1;
+			_minesMap[i % size][i / size] = 1;
+			n--;
+		}
+	}
+}
+
+void Game::renderGameData(int size)
+{
+	for (int i = 0; i < size; i++) {
+		for (int j = 0; j < size; j++) {
+			std::cout << _minesMap[i][j] << " ";
+		}
+		std::cout << '\n';
+	}
+	Sleep(20000);
 }
 
 void Game::drawGame(int size) {//vẽ ra bảng game
--- Minesweeper/Game.h
+++ Minesweeper/Game.h
@@ -1,22 +1,26 @@
 #pragma once
 
 #include "Common.h"
+#include <iostream>
 
 const int CELL_HEIGHT = 3;
 const int CELL_LENGTH = 3;
 
 class Game {
 private:
-	int size;
-	int mumOfMines;
-	int numOfFlags;
-	int flagsPlaced = 0;
-	int cellsDigged = 0;
+	int _size = 0;
+	int _numOfMines = 0;
+	int _numOfFlags = 0;
+	int _flagsPlaced = 0;
+	int _cellsDigged = 0;
+	bool** _minesMap = nullptr;
 
 public:
 	Game();
 	~Game();
 
 	void setUpGame(int);
+	void generateGameData(int);
+	void renderGameData(int);
 	void drawGame(int);
 };
